65d052ad3aef9585edb14557b3b17f2b1cf1f121
hasim13ab <hasim13ab@gmail.com>
2025-09-02 13:42:37 +0000

    android license

--- android/app/src/main/kotlin/com/example/scanairz/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/scanairz/MainActivity.kt
@@ -1,5 +1,12 @@
 package com.example.scanairz
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+import androidx.annotation.NonNull
+import io.flutter.embedding.engine.FlutterEngine
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterFragmentActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+    }
+}
