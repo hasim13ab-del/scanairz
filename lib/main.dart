1d1818c1277664e33fe3a53394e81b826f303db6
hasim13ab <hasim13ab@gmail.com>
2025-09-02 12:11:49 +0000

    Changes before Firebase Studio auto-run

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,13 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:scanairz/features/app_shell.dart';
 import 'theme/app_theme.dart';
 import 'utils/error_handler.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_app_check/firebase_app_check.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  await FirebaseAppCheck.instance.activate();
   ErrorHandler.init();
   runApp(const ProviderScope(child: ScanairzApp()));
 }
